0d850141e9f91eff64a8393c6e9f7d64236fa964
Dirk van Oosterbosch <developer@irlabs.nl>
2016-11-22 12:51:30 +0100

    Additions to the rogerbot

--- rogerbot.py
+++ rogerbot.py
@@ -17,6 +17,10 @@ def output(s):
 
 # Setup and Response function
 def setup():
+    global askedCounter
+    global orderCounter
+    askedCounter = 0
+    orderCounter = 0
     output("Hello, my name is Roger.")
     sleep(1)
     output("What's up?")
@@ -24,11 +28,13 @@ def setup():
 
 def response(input):
     # print(input)
-    if not respondToTrigger(input):
-        output(randomResponse())
+    if respondToTrigger(input):
+        pass
+    else:
+        output(defaultOrderedResponse())
 
 
-def randomResponse():
+def defaultOrderedResponse():
     answers = [
         "Ok",
         "Roger",
@@ -41,11 +47,31 @@ def randomResponse():
         "I agree",
         "Absolutely"
     ]
+    response = answers[orderCounter]
+    orderCounter += 1
+    if orderCounter >= len(answers):
+        orderCounter = 0
+    return response
+
+
+def randomAssignments():
+    answers = [
+        "The assignment is to design a chatbot.",
+        "Design a chatbot",
+        "A Chatbot, You make it!",
+        "Make it work, this chatbot."
+    ]
     return random.choice(answers)
 
 
 def respondToTrigger(input):
-    if "assignment" in input:
-        output("The assigment is to design a chatbot.")
-        return True
+    global askedCounter
+    triggers = ["assignment", "opdracht", "what do I do"]
+    for t in triggers:
+        if t in input:
+            if askedCounter > 0:
+                output("Why do you ask me this again?")
+            output(randomAssignments())
+            askedCounter += 1
+            return True
     return False
